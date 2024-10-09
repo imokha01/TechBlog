19cf36a1aaa28563a2a029df06f1f2ec11ca70b9
Ahmed Wahab <ahmed.wahab.imokha@gmail.com>
2024-10-09 16:55:05 +0100

    created pages and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,22 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import SignIn from "./pages/Signin"
+import SignUp from "./pages/Signup"
+import Dashboard from "./pages/Dashboard"
+import Projects from "./pages/Projects"
 
 function App() {
 
   return (
-    <div>
-      <p className="text-3xl text-red-500">Hello World</p>
-    </div>
+    <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/projects' element={<Projects />} />
+    </Routes>
   )
 }
 
