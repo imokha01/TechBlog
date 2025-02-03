08765b4ef1bf71c843ef82116914bf8db02647ce
Ahmed Wahab <ahmed.wahab.imokha@gmail.com>
2025-02-03 17:26:26 +0100

    rearranged the auth.controller codes

--- backend/api/controllers/auth.controller.js
+++ backend/api/controllers/auth.controller.js
@@ -7,34 +7,39 @@ export const signup = async (req, res, next) =>{
   // Validate the request body data
   const { username, email, password } = req.body;
 
-  if (
-    !username ||
-    !email ||
-    !password ||
-    username === "" ||
-    email === "" ||
-    password === ""
-  ) {
-    next(errorHandler(400, "All fields are required"));
-  }
-
-  //encrypt the password before saving to the database
-  const hashedPassword = bcryptjs.hashSync(password, 10);
-
-  //Create a new user with the given username, email and password
-  const newUser = new User({
-    username,
-    email,
-    password: hashedPassword,
-  });
-
-  //Send a success response with a message
   try {
-    await newUser.save(); //Save the new user to the database
+    //Check if all fields are provided
+    if (
+      !username ||
+      !email ||
+      !password ||
+      username === "" ||
+      email === "" ||
+      password === ""
+    ) {
+      next(errorHandler(400, "All fields are required"));
+    }
+  
+    //encrypt the password before saving to the database
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+  
+    //Create a new user with the given username, email and password
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+  
+    //Save the new user to the database
+    await newUser.save(); 
+
+    //send success response
     res.json({ message: "signup successful" });
     console.log("User created successfully".green.bold);
   } catch (error) {
     console.log(error);
-    next(error); //the next error is the error from the middleware
+
+    //Handle errors
+    next(error); 
   }
 };
