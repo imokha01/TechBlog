3fa0062e53b253ab3ea2c169e192eea0a8c9bacf
Ahmed Wahab <ahmed.wahab.imokha@gmail.com>
2025-02-11 10:48:39 +0100

    added the footer UI

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,70 @@
+import { Footer } from "flowbite-react"
+import { Link } from "react-router-dom"
+import {BsFacebook, BsInstagram, BsTwitter, BsGithub, BsDribbble} from "react-icons/bs";
+
+const FooterCom = () => {
+  return (
+    <Footer className="border border-t-8 border-teal-500">
+      <div className="w-full ">
+        <div className="m-4 pt-4">
+        <Link to="/" className="text-3xl font-bold dark:text-white">
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+              TechGuru
+            </span>
+            Blog
+        </Link>
+        </div>
+        <div className="grid w-full grid-cols-2 gap-8 px-8 py-8 md:grid-cols-4">
+          <div>
+            <Footer.Title title="Company" />
+            <Footer.LinkGroup col>
+              <Footer.Link href="#">About</Footer.Link>
+              <Footer.Link href="#">Careers</Footer.Link>
+              <Footer.Link href="#">Brand Center</Footer.Link>
+              <Footer.Link href="#">Blog</Footer.Link>
+            </Footer.LinkGroup>
+          </div>
+          <div>
+            <Footer.Title title="help center" />
+            <Footer.LinkGroup col>
+              <Footer.Link href="#">Discord Server</Footer.Link>
+              <Footer.Link href="#">Twitter</Footer.Link>
+              <Footer.Link href="#">Facebook</Footer.Link>
+              <Footer.Link href="#">Contact Us</Footer.Link>
+            </Footer.LinkGroup>
+          </div>
+          <div>
+            <Footer.Title title="legal" />
+            <Footer.LinkGroup col>
+              <Footer.Link href="#">Privacy Policy</Footer.Link>
+              <Footer.Link href="#">Licensing</Footer.Link>
+              <Footer.Link href="#">Terms &amp; Conditions</Footer.Link>
+            </Footer.LinkGroup>
+          </div>
+          <div>
+            <Footer.Title title="download" />
+            <Footer.LinkGroup col>
+              <Footer.Link href="#">iOS</Footer.Link>
+              <Footer.Link href="#">Android</Footer.Link>
+              <Footer.Link href="#">Windows</Footer.Link>
+              <Footer.Link href="#">MacOS</Footer.Link>
+            </Footer.LinkGroup>
+          </div>
+        </div>
+        <Footer.Divider className="my-0  lg:my-0" />
+        <div className="w-full  py-4 px-4 sm:flex sm:items-center sm:justify-between sm:py-4">
+          <Footer.Copyright href="#" by="Ahmed Wahab™" year={new Date().getFullYear()} />
+          <div className=" mt-2 flex space-x-6 sm:mt-0 sm:justify-center">
+            <Footer.Icon href="#" icon={BsFacebook} />
+            <Footer.Icon href="#" icon={BsInstagram} />
+            <Footer.Icon href="#" icon={BsTwitter} />
+            <Footer.Icon href="#" icon={BsGithub} />
+            <Footer.Icon href="#" icon={BsDribbble} />
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
+}
+
+export default FooterCom
