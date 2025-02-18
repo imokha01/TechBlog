362b6fc7b0ed87204a6db35087f33053799b7dce
Ahmed Wahab <ahmed.wahab.imokha@gmail.com>
2025-02-18 11:29:16 +0100

    Completed the Dashboard Sidebar

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,42 @@
+import { useState, useEffect } from "react"
+import { useLocation } from "react-router-dom"
+import DashSidebar from "../components/DashSidebar"
+import DashProfile from "../components/DashProfile"
+
 
 const Dashboard = () => {
+  //! Use useLocation hook to access location object, which contains the current URL information.
+  const location = useLocation()
+  console.log(location)
+
+  //! Use useEffect hook to manage the state of the 'tab' value extracted from the URL
+    const [tab, setTab] = useState('')
+
+
+
+  //TODO create a new Dashboard instance with the specified location and tab location.
+  useEffect(() =>{
+    // Extract the 'tab' value from the URL search parameters.
+    const urlParams = new URLSearchParams(location.search)
+  
+
+    // Set the 'tab' state with the extracted value.  
+    const tabFromUrl = urlParams.get('tab')
+    console.log(tabFromUrl)
+
+    // Update the state with the extracted tab value
+    if (tabFromUrl) {
+      setTab(tabFromUrl)
+    }
+  }, [location.search])
   return (
-    <div>
-      Dashboard
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className="md:w-56">
+        {/* Sidebar */}
+        <DashSidebar/>
+      </div>
+         {/* profile */}
+         {tab === 'profile' && <DashProfile />}
     </div>
   )
 }
